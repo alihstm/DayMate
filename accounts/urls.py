d7b2bad9eb589e9d1742f98034808f87e25a5730
RoeinYousefi <roeinyousefi@gmail.com>
2025-03-24 14:15:23 +0330

    feat: create new register , login , logout , profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,14 +2,41 @@ from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, LoginView, LogoutView, UserProfileView
+from django.contrib import admin
+
+
 
 urlpatterns = [
-    path('auth/register/' , RegisterView.as_view() , name = 'register'),
+    path('admin/', admin.site.urls),
+    path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
-    path('auth/user/' , UserDetailsView.as_view() , name='user_detail'),
-
-    # JWT token
-    path('auth/token/' , TokenObtainPairView.as_view(), name='token_obtaim'),
-    path('auth/token/refresh/' , TokenRefreshView.as_view() , name='token_refresh'),
+    path('auth/profile/', UserProfileView.as_view(), name='profile'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# urlpatterns = [
+#     path('auth/register/' , RegisterView.as_view() , name = 'register'),
+#     path('auth/login/', LoginView.as_view(), name='login'),
+#     path('auth/logout/', LogoutView.as_view(), name='logout'),
+#     path('auth/all_user/' , GetUser.as_view() , name='user_detail'),
+#     path('auth/profile_user/' , UserProfileView.as_view() , name='user_detail'),
+
+
+#     # JWT token
+#     path('auth/token/' , TokenObtainPairView.as_view(), name='token_obtaim'),
+#     path('auth/token/refresh/' , TokenRefreshView.as_view() , name='token_refresh'),
+# ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated , AllowAny
+from django.contrib.auth import get_user_model , authenticate, logout
 
-# Create your views here.
+from .serializers import UserRegisterSerializers, UserLoginSerializer, UserProfileSerializer
+
+import requests
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializers
+    permission_classes = [AllowAny]
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        user = authenticate(
+            username=serializer.validated_data['username'],
+            password=serializer.validated_data['password']
+        )
+        
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email
+                }
+            })
+        return Response({'error': 'اطلاعات اشتباهه مشتی'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        logout(request)
+        return Response({'message': 'Successfully logged out.'})
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
